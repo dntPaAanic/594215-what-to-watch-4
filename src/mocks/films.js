16c1c7a6928aee9ba419344bdd92493106522aee
Vitaly Mes <volkondr@ya.ru>
2020-06-16 11:10:02 +0300

    Создает моки для карточки фильма

---
+++ src/mocks/films.js
@@ -0,0 +1,52 @@
+const films = [
+  {
+    title: `The Grand Budapest Hotel`,
+    image: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    genre: `Drama`,
+    releaseDate: `2001`,
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    image: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    genre: `Comedy`,
+    releaseDate: `2014`,
+  },
+  {
+    title: `Macbeth`,
+    image: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    genre: `Comedy`,
+    releaseDate: `2000`,
+  },
+  {
+    title: `Aviator`,
+    image: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    genre: `Comedy`,
+    releaseDate: `2012`,
+  },
+  {
+    title: `We need to talk about Kevin`,
+    image: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    genre: `Crime`,
+    releaseDate: `2018`,
+  },
+  {
+    title: `What We Do in the Shadows`,
+    image: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    genre: `Drama`,
+    releaseDate: `2005`,
+  },
+  {
+    title: `Pulp Fiction`,
+    image: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    genre: `Drama`,
+    releaseDate: `2003`,
+  },
+  {
+    title: `No Country for Old Men`,
+    image: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    genre: `Comedy`,
+    releaseDate: `2014`,
+  },
+];
+
+export default films;
